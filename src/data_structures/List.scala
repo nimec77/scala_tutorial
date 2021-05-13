d0a5cdb73502f2ab11bc53216489510561fb1516
comrade77 <nimec77@gmail.com>
2021-05-13 10:33:48 +0700

    Закончил 3.3 Data sharing in functional data structures

--- src/data_structures/List.scala
+++ src/data_structures/List.scala
@@ -1,5 +1,7 @@
 package data_structures
 
+import scala.annotation.tailrec
+
 
 sealed trait List[+A]
 
@@ -23,6 +25,59 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, t) => t
+    }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+    }
+
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
+
+  def init2[A](l: List[A]): List[A] = {
+    import collection.mutable.ListBuffer
+    val buf = new ListBuffer[A]
+
+    @tailrec
+    def go(cur: List[A]): List[A] = cur match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_, Nil) => List(buf.toList: _*)
+      case Cons(h, t) => buf += h; go(t)
+    }
+
+    go(l)
+  }
+
   def main(args: Array[String]): Unit = {
     val x = List(1, 2, 3, 4, 5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
@@ -31,5 +86,7 @@ object List {
       case _ => 101
     }
     println(x)
+    val y = tail(List(1, 2, 3, 4, 5))
+    println(y)
   }
 }
