bb3d1d1d8867ca17e81251b42820e414e9ebf3bb
comrade77 <nimec77@gmail.com>
2021-05-14 11:17:47 +0700

    Закончил 3.4
    Recursion over lists and generalizing to higher-order functions

--- src/data_structures/List.scala
+++ src/data_structures/List.scala
@@ -1,6 +1,7 @@
 package data_structures
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 
 sealed trait List[+A]
@@ -78,6 +79,137 @@ object List {
     go(l)
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
+
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, acc) => acc + 1)
+
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+
+  def sum3(l: List[Int]): Int =
+    foldLeft(l, 0)(_ + _)
+
+  def product3(l: List[Double]): Double =
+    foldLeft(l, 1.0)(_ * _)
+
+  def length2[A](l: List[A]): Int =
+    foldLeft(l, 0)((acc, _) => acc + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((t, h) => Cons(h, t))
+
+
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(l), z)((b, a) => f(a, b))
+
+  def foldRightVieFoldLeft_1[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
+
+  def foldLeftVieFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(l, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
+
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil: List[A])(append)
+
+  def add1(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((h, t) => Cons(h + 1, t))
+
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
+
+  def map[A, B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def map_1[A, B](l: List[A])(f: A => B): List[B] =
+    foldRightViaFoldLeft(l, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def map_2[A, B](l: List[A])(f: A => B): List[B] = {
+    val buf = new ListBuffer[B]
+
+    @tailrec
+    def go(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => buf += f(h); go(t)
+    }
+
+    go(l)
+
+    List(buf.toList: _*)
+  }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRight(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def filter_1[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRightViaFoldLeft(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def filter_2[A](l: List[A])(f: A => Boolean): List[A] = {
+    val buf = new ListBuffer[A]
+
+    @tailrec
+    def go(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => if (f(h)) buf += h; go(t)
+    }
+
+    go(l)
+
+    List(buf.toList: _*)
+  }
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+    concat(map(l)(f))
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def addPairwise(as: List[Int], bs: List[Int]): List[Int] =
+    (as, bs) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+    }
+
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = (as, bs) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
+
+  @tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean =
+    (l, prefix) match {
+      case (_, Nil) => true
+      case (Cons(h1, t1), Cons(h2, t2)) if h1 == h2 => startsWith(t1, t2)
+      case _ => false
+    }
+
+  @tailrec
+  def hasSubsequence[A](l: List[A], sub: List[A]): Boolean =
+    l match {
+      case Nil => sub == Nil
+      case _ if startsWith(l, sub) => true
+      case Cons(_, t) => hasSubsequence(t, sub)
+    }
+
   def main(args: Array[String]): Unit = {
     val x = List(1, 2, 3, 4, 5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
@@ -88,5 +220,8 @@ object List {
     println(x)
     val y = tail(List(1, 2, 3, 4, 5))
     println(y)
+
+    val z = filter(List(1, 2, 3, 4, 5))(_ % 2 == 0)
+    println(z)
   }
 }
