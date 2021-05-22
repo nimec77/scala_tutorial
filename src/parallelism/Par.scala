755f5e462194137094cbb7e8437d57afec331f25
comrade77 <nimec77@gmail.com>
2021-05-22 16:44:37 +0700

    Закончил 7.2
    Picking a representation

---
+++ src/parallelism/Par.scala
@@ -0,0 +1,34 @@
+package parallelism
+
+import java.util.concurrent._
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit): A = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(() => a(es).get)
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+}
