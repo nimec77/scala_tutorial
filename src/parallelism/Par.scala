24cf9b3d8caffc30242721bb3c817a8f46930ee2
comrade77 <nimec77@gmail.com>
2021-05-23 13:07:58 +0700

    Закончил 7.3
    Refining the API

--- src/parallelism/Par.scala
+++ src/parallelism/Par.scala
@@ -10,13 +10,13 @@ object Par {
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
   private case class UnitFuture[A](get: A) extends Future[A] {
-    def isDone = true
+    override def isDone = true
 
-    def get(timeout: Long, units: TimeUnit): A = get
+    override def get(timeout: Long, units: TimeUnit): A = get
 
-    def isCancelled = false
+    override def isCancelled = false
 
-    def cancel(evenIfRunning: Boolean): Boolean = false
+    override def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
@@ -31,4 +31,39 @@ object Par {
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
+
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
+    map(parList)(_.sorted)
+
+  def sequence_simple[A](l: List[Par[A]]): Par[List[A]] =
+    l.foldRight[Par[List[A]]](unit(List()))((h, t) => map2(h, t)(_ :: _))
+
+  def sequenceRight[A](as: List[Par[A]]): Par[List[A]] = as match {
+    case Nil => unit(Nil)
+    case h :: t => map2(h, fork(sequenceRight(t)))(_ :: _)
+  }
+
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l, r) = as.splitAt(as.length / 2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
+  }
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] =
+    map(sequenceBalanced(as.toIndexedSeq))(_.toList)
+
+  def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] =
+      l map asyncF((a: A) => if (f(a)) List(a) else List())
+    map(sequence(pars))(_.flatten)
+  }
 }
+
