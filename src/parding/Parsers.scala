78a4abb31e0a6b260ae729e8496644fc29abf371
comrade77 <nimec77@gmail.com>
2021-05-30 11:03:25 +0700

    Закончил 9.2
    A possible algebra

--- src/parding/Parsers.scala
+++ src/parding/Parsers.scala
@@ -1,12 +1,16 @@
 package parding
 
+import testing.Prop._
+import testing._
+
 import scala.language.implicitConversions
+import scala.util.matching.Regex
 
 trait Parsers[Parser[+_]] {
   self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  def or[A](s1: Parser[A], s: Parser[A]): Parser[A]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
   implicit def string(s: String): Parser[String]
 
@@ -25,13 +29,43 @@ trait Parsers[Parser[+_]] {
 
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _) or succeed(List())
+
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+    for {a <- p; b <- p2} yield f(a, b)
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  implicit def regex(r: Regex): Parser[String]
+
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    flatMap(p)(a => map(p2)(b => (a, b)))
+
+  def lablel[A](msg: String)(p: Parser[A]): Parser[A]
 
   case class ParserOps[A](p: Parser[A]) {
+
     def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def many: Parser[List[A]] = self.many(p)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] =
+      self.flatMap(p)(f)
+
+  }
+
+  object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
   }
 }
 
