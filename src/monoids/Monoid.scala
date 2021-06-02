82179c1923bf1315d2fc65161eea2f5aac55d60b
comrade77 <nimec77@gmail.com>
2021-06-02 10:17:48 +0700

    Закончил 10.1
    What is a monoid?

---
+++ src/monoids/Monoid.scala
@@ -0,0 +1,79 @@
+package monoids
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+
+  def zero: A
+}
+
+object Monoid {
+  val stringMonoid: Monoid[String] = new Monoid[String] {
+    def op(a1: String, a2: String): String = a1 + a2
+
+    val zero = ""
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+    override def zero: List[A] = Nil
+
+  }
+
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+
+    override def zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = true
+  }
+
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
+    override def zero: Option[A] = None
+  }
+
+  def dual[A](m: Monoid[A]): Monoid[A] = new Monoid[A] {
+    override def op(a1: A, a2: A): A = m.op(a1, a2)
+
+    override def zero: A = m.zero
+  }
+
+  def firstOptionMonoid[A]: Monoid[Option[A]] = optionMonoid[A]
+
+  def lastOptionMonoid[A]: Monoid[Option[A]] = dual(firstOptionMonoid)
+
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a1 compose a2
+
+    override def zero: A => A = _
+  }
+
+  import testing._
+  import Prop._
+
+  def monoidLaw[A](m: Monoid[A], gen: Gen[A]): Prop =
+    forAll(for {
+      x <- gen
+      y <- gen
+      z <- gen
+    } yield (x, y, z))(p => m.op(p._1, m.op(p._2, p._3)) == m.op(m.op(p._1, p._2), p._3)) &&
+      forAll(gen)((a: A) => m.op(a, m.zero) == a && m.op(m.zero, a) == a)
+}
